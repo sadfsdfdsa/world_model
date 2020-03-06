10208ce25a2111fbfc616d90431ef6debdf6e64d
Shuvaev Artem <sha@get-net.ru>
2020-03-06 18:18:50 +0500

    minor 0.01.1

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ EngineConfig.ModelsConfig.HerbivoreConfig.number_max = 1
 EngineConfig.ModelsConfig.HerbivoreConfig.health_min = 4
 EngineConfig.ModelsConfig.HerbivoreConfig.health_max = 6
 
+EngineConfig.ModelsConfig.HerbivoreConfig.age_min = 20
+EngineConfig.ModelsConfig.HerbivoreConfig.age_max = 35
+
 EngineConfig.ModelsConfig.FoodConfig.number_min = 0
 EngineConfig.ModelsConfig.FoodConfig.number_max = 2
 
-EngineConfig.ModelsConfig.FoodConfig.spawn_number_min = 1
-EngineConfig.ModelsConfig.FoodConfig.spawn_number_max = 2
+EngineConfig.ModelsConfig.FoodConfig.spawn_number_min = 2
+EngineConfig.ModelsConfig.FoodConfig.spawn_number_max = 3
 
 EngineConfig.ModelsConfig.FoodConfig.age_min = 5
 EngineConfig.ModelsConfig.FoodConfig.age_max = 5
