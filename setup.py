13321dca1a2d2221dc99247d517a47f9df6dc166
Shuvaev Artem <sha@get-net.ru>
2020-03-06 17:31:52 +0500

    asd

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Package with world model for simulation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/sadfsdfdsa/world_model",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
